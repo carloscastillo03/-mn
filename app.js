30f42dd4b52e43cda739cb504b580bf053c74a30
Carlos Castillo <carloscastillo03@hotmail.com>
2020-04-09 00:32:29 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+if (screen.width<=1080) {
+  window.location="http://ix.sk/4pJKT";
+}
+
+
+function muestra_oculta(){
+if (screen.width <= 720) {
+  document.getElementById("ocultar").style.display = "none";
+  document.getElementById("visible").style.display = "block";
+
+}
+console.log("pppp22");
+}
+
+muestra_oculta();
+
